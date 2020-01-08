5ef478f11bf56070ee10c510ed7715b834689e47
zakpayne8283 <zakpayne8283@gmail.com>
2020-01-08 12:49:28 -0500

    Commit to fix one text thing

--- src/components/App.js
+++ src/components/App.js
@@ -53,8 +53,8 @@ export class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h1>Product Cards</h1>
+        <h2>Now with infinite scroll!</h2>
         <div
           className="cards-container"
           style={{
